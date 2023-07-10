819afcbde80cb2b64f84c6acd377645d614dbbf3
Zella <sergioluiz.zella@live.com>
2023-07-10 18:03:13 -0300

    estruturnado listagem de pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.scss']
+  styleUrls: ['./listar-pensamento.component.scss'],
 })
 export class ListarPensamentoComponent {
-
+  listaPensamentos = [];
 }
